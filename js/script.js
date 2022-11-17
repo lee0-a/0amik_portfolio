7e1f7a964ee03c5ef97ca605c695555756d7fcff
Ayeong Kim <fay3349@naver.com>
2022-11-17 15:09:17 +0900

    second commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
 $(document).ready(function(){
 
-    $(".nav__menu li a").on("click",function(e){
-      e.preventDefault();
-      let thisTarget=$(this).attr("href");
-      $(window).scrollTop($(thisTarget).offset().top);
+  let $menu     = $('.nav__menu li'),
+  $contents = $('.scroll'),
+  $doc      = $('html, body');
+$(function () {
+  // 해당 섹션으로 스크롤 이동
+  $menu.on('click','a', function(e){
+      let $target = $(this).parent(),
+          idx     = $target.index(),
+          section = $contents.eq(idx),
+          offsetTop = section.offset().top;
+      $doc.stop()
+              .animate({
+                  scrollTop :offsetTop
+              }, 800);
+      return false;
+  });
+});
+// menu class 추가
+$(window).scroll(function(){
+  let scltop = $(window).scrollTop();
+  $.each($contents, function(idx, item){
+    let $target   = $contents.eq(idx),
+          i         = $target.index(),
+          targetTop = $target.offset().top;
 
-    });
+      if (targetTop <= scltop) {
+          $menu.removeClass('on');
+          $menu.eq(idx).addClass('on');
+      }
+      if (!(200 <= scltop)) {
+          $menu.removeClass('on');
+      }
+  })
+
+
+
+// Go to the TOP
+let btnTop = $('.btn-top');
+btnTop.on('click','a', function(e){
+  e.preventDefault();
+  $doc.stop()
+          .animate({
+              scrollTop : 0
+          },800)
+});
+
+}(window, window.jQuery));
+
+
+
 
     $('.section01_me>a').click(function(){
         $('#modalBG').addClass("active");
@@ -22,16 +66,18 @@ $(document).ready(function(){
         $(".pub01 div span, .des01 div span, .des02 div span, .des03 div span").show();
       });
 
+      
+
       $(".pub_li").click(function(){
         $(".pubBox").show();
-        $(".desBox").hide();
+        $(".desBox, .swiper-button-next, .swiper-button-prev").hide();
       });
       $(".des_li").click(function(){
-        $(".desBox").show();
+        $(".desBox, .swiper-button-next, .swiper-button-prev").show();
         $(".pubBox").hide();
       });
       
-      var swiper = new Swiper(".mySwiper", {
+      let swiper = new Swiper(".mySwiper", {
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
